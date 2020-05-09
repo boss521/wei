b5e42303a266f2c70c9eaf5c97f196d69476d115
sun yuan <595086728@qq.com>
2020-05-09 17:41:56 +0800

    common commit

--- src/pages/addNewUser/index.vue
+++ src/pages/addNewUser/index.vue
@@ -1,20 +1,23 @@
 <template>
     <div class="add-new-user">
         <Form
-            :model="ruleForm"
-            :rules="ruleObj"
-            status-icon
-            ref="ruleForm"
-            :label-width="100"
-            class="demo-ruleForm form-box"
+                :model="ruleForm"
+                :rules="ruleObj"
+                status-icon
+                ref="ruleForm"
+                :label-width="100"
+                class="demo-ruleForm form-box"
         >
+            <FormItem label="姓 名" prop="client_name">
+                <Input class="typeValue" type="text" v-model="ruleForm.client_name"/>
+            </FormItem>
             <FormItem label="添加类型" prop="clientType">
-                <Select v-model="ruleForm.clientType" placeholder="请选择类型">
+                <Select v-model="ruleForm.client_type" placeholder="请选择类型">
                     <Option
-                        v-for="item in options"
-                        :key="item.value"
-                        :label="item.label"
-                        :value="item.value"
+                            v-for="item in options"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value"
                     />
                 </Select>
             </FormItem>
@@ -30,66 +33,71 @@
 </template>
 
 <script>
-  export default {
-    name: "index",
-    data () {
-      return {
-        value: '',
-        options: [{
-          value: 'wx',
-          label: '微信'
-        }, {
-          value: 'phone',
-          label: '手机号'
-        }],
-        ruleForm: {
-          clientType: '',
-          client_id: '',
-          startTime: new Date().getTime(),
-          endTime: new Date().getTime()
+    export default {
+        name: "index",
+        data() {
+            return {
+                options: [{
+                    value: 'wx',
+                    label: '微信'
+                }, {
+                    value: 'phone',
+                    label: '手机号'
+                }],
+                ruleForm: {
+                    client_name: '',
+                    client_type: '',
+                    client_id: '',
+                    start_time: new Date().getTime(),
+                    end_time: new Date().getTime() + 31536000000
+                },
+                ruleObj: {
+                    client_name: [
+                        {required: true, message: '请输入姓名', trigger: 'blur'},
+                        {message: '请输入姓名', trigger: 'blur'}
+                    ],
+                    client_type: [
+                        {required: true, message: '请选择类型', trigger: 'blur'},
+                        {message: '请选择类型', trigger: 'blur'}
+                    ],
+                    client_id: [
+                        {required: true, message: '请输入类型的值', trigger: 'blur'},
+                        {min: 3, max: 11, message: '长度在 3 到 11 个字符', trigger: 'blur'}
+                    ]
+                }
+            }
         },
-        ruleObj: {
-          clientType: [
-            {required: true, message: '请选择类型', trigger: 'blur'},
-            {message: '请选择类型', trigger: 'blur'}
-          ],
-          client_id: [
-            {required: true, message: '请输入类型的值', trigger: 'blur'},
-            {min: 3, max: 11, message: '长度在 3 到 11 个字符', trigger: 'blur'}
-          ]
+        methods: {
+            submitForm(formName) {
+                if (!this.ruleForm.client_name || !this.ruleForm.client_type || !this.ruleForm.client_id) {
+                    this.$Message.warning('请完善信息')
+                    return false
+                }
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        console.log(this)
+                        this.$http.post('/addNewUser', this.ruleForm).then((response) => {
+                            console.log(response)
+                            if (response.data.code === 200) {
+                                this.$Message.success('添加成功,列表中查看')
+                            } else {
+                                this.$Message.error(response.data.message)
+                                this.resetForm('ruleForm')
+                            }
+                        }).catch(function (error) {
+                            console.log(error)
+                        })
+                    } else {
+                        this.$Message.error('添加失败')
+                        return false
+                    }
+                })
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields()
+            }
         }
-      }
-    },
-    methods: {
-      submitForm (formName) {
-        if (this.ruleForm.clientType === '' || this.ruleForm.client_id === '') {
-          this.$Message.warning('请完善信息')
-          return false
-        }
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            this.$http.post('/addNewUser', this.ruleForm).then((response) => {
-              console.log(response)
-              if (response.data.code === 200) {
-                this.$Message.success('添加成功,列表中查看')
-              } else {
-                this.$Message.error(response.data.message)
-                this.resetForm('ruleForm')
-              }
-            }).catch(function (error) {
-              console.log(error)
-            })
-          } else {
-            this.$Message.error('添加失败')
-            return false
-          }
-        })
-      },
-      resetForm (formName) {
-        this.$refs[formName].resetFields()
-      }
     }
-  }
 </script>
 
 <style scoped>
--- src/pages/allUserList/index.vue
+++ src/pages/allUserList/index.vue
@@ -13,18 +13,18 @@
         <div style="margin: 10px;overflow: hidden">
             <div style="float: right;">
                 <Page
-                    show-total
-                    :total="totalData.length"
-                    :current="currentPage"
-                    :page-size="5"
-                    @on-change="changePage"
+                        show-total
+                        :total="totalData.length"
+                        :current="currentPage"
+                        :page-size="5"
+                        @on-change="changePage"
                 />
             </div>
         </div>
         <Modal
-            v-model="showModal"
-            :loading="loading"
-            @on-ok="deleteLine"
+                v-model="showModal"
+                :loading="loading"
+                @on-ok="deleteLine"
         >
             <p>确定要删除吗？</p>
         </Modal>
@@ -32,127 +32,138 @@
 </template>
 
 <script>
-  export default {
-    name: "allUserList",
-    data () {
-      return {
-        currentPage: 1,// 当前页
-        totalData: [], // 数据
-        showModal: false, // 显示对话框
-        loading: true,
-        optIndex: -1, // 要操作得index
-        optId: -1, // 要操作得id
-        columns: [
-          {
-            title: 'id',
-            key: 'id'
-          },
-          {
-            title: '微信号',
-            key: 'wx_id'
-          },
-          {
-            title: '手机号',
-            key: 'phone'
-          },
-          {
-            title: '授权书',
-            key: 'licence_snapshot',
-            render: (h, params) => {
-              return h('a', {
-                attrs: {
-                  href: params.row.licence_snapshot, target: '_blank', title: params.row.licence_snapshot
-                }
-              }, [h('img', {
-                attrs: {
-                  src: params.row.licence_snapshot, style: 'width: 90px;border-radius: 2px;cursor:pointer'
-                }
-              })], params.row.licence_snapshot)
+    export default {
+        name: "allUserList",
+        data() {
+            return {
+                currentPage: 1,// 当前页
+                totalData: [], // 数据
+                showModal: false, // 显示对话框
+                loading: true,
+                optIndex: -1, // 要操作得index
+                optId: -1, // 要操作得id
+                columns: [
+                    {
+                        title: 'id',
+                        key: 'id'
+                    },
+                    {
+                        title: '姓名',
+                        key: 'nickname'
+                    },
+                    {
+                        title: '微信号',
+                        key: 'wx_id'
+                    },
+                    {
+                        title: '手机号',
+                        key: 'phone'
+                    },
+                    {
+                        title: '授权书',
+                        key: 'licence_snapshot',
+                        render: (h, params) => {
+                            return h('a', {
+                                attrs: {
+                                    href: params.row.licence_snapshot,
+                                    target: '_blank',
+                                    title: params.row.licence_snapshot
+                                }
+                            }, [h('img', {
+                                attrs: {
+                                    src: params.row.licence_snapshot,
+                                    style: 'width: 90px;border-radius: 2px;cursor:pointer'
+                                }
+                            })], params.row.licence_snapshot)
+                        }
+                    },
+                    {
+                        title: '起始时间',
+                        key: 'start_time',
+                        width: 150,
+                        align: 'center'
+                    },
+                    {
+                        title: '失效时间',
+                        key: 'end_time',
+                        width: 150,
+                        align: 'center'
+                    },
+                    {
+                        title: '操作',
+                        slot: 'action',
+                        width: 150,
+                        align: 'center'
+                    }
+                ]
             }
-          },
-          {
-            title: '起始时间',
-            key: 'start_time',
-            width: 150,
-            align: 'center'
-          },
-          {
-            title: '失效时间',
-            key: 'end_time',
-            width: 150,
-            align: 'center'
-          },
-          {
-            title: '操作',
-            slot: 'action',
-            width: 150,
-            align: 'center'
-          }
-        ]
-      }
-    },
-    mounted () {
-      this.getData()
-    },
-    computed: {
-      renderData () {
-        let i = this.currentPage - 1
-        return this.totalData.slice(i * 5, i * 5 + 5)
-      }
-    },
-    methods: {
-      deleteLine () {
-        this.$http.post('/deleteUser?id=' + this.optId).then(res => {
-          if (res.data.code === 200) {
-            this.showModal = false
-            this.totalData.splice(this.optId, 1)
-            this.$Message.success('删除成功')
-            this.totalData = res.data.data
-          } else {
-            this.$Message.error('获取数据失败')
-          }
-        }).catch(err => {
-          console.error(err)
-        })
-      },
-      getData () {
-        this.$http.get('/allUsers').then(res => {
-          if (res.data.code === 200) {
-            this.totalData = res.data.data
-            this.totalData.forEach(item => {
-              if (!item.wx_id) {
-                item.wx_id = '无'
-              }
-              if (!item.phone) {
-                item.phone = '无'
-              }
-              if (!item.licence_snapshot) {
-                item.licence_snapshot = '无'
-              }
-            })
-          } else {
-            this.$Message.error('获取数据失败')
-          }
-        }).catch(err => {
-          console.error(err)
-        })
-      },
-      show (index) {
-        this.$Modal.info({
-          title: 'User Info',
-          content: `id：${this.totalData[index].id}<br>wx_id：${this.totalData[index].wx_id}<br>phone：${this.totalData[index].phone}<br>licence_snapshot：${this.totalData[index].licence_snapshot}`
-        })
-      },
-      remove (index, row) {
-        this.showModal = true
-        this.optIndex = index
-        this.optId = row.id
-      },
-      changePage (p) {
-        this.currentPage = p
-      }
+        },
+        mounted() {
+            this.getData()
+        },
+        computed: {
+            renderData() {
+                let i = this.currentPage - 1
+                return this.totalData.slice(i * 5, i * 5 + 5)
+            }
+        },
+        methods: {
+            deleteLine() {
+                this.$http.post('/deleteUser?id=' + this.optId).then(res => {
+                    if (res.data.code === 200) {
+                        this.showModal = false
+                        this.totalData.splice(this.optId, 1)
+                        this.$Message.success('删除成功')
+                        this.totalData = res.data.data
+                    } else {
+                        this.$Message.error('获取数据失败')
+                    }
+                }).catch(err => {
+                    console.error(err)
+                })
+            },
+            getData() {
+                this.$http.get('/allUsers').then(res => {
+                    console.log(res)
+                    if (res.data.code === 200) {
+                        this.totalData = res.data.data
+                        this.totalData.forEach(item => {
+                            if (!item.wx_id) {
+                                item.wx_id = '无'
+                            }
+                            if (!item.phone) {
+                                item.phone = '无'
+                            }
+                            if (!item.licence_snapshot) {
+                                item.licence_snapshot = '无'
+                            }
+                            if (!item.nickname) {
+                                item.nickname = '无'
+                            }
+                        })
+                    } else {
+                        this.$Message.error('获取数据失败')
+                    }
+                }).catch(err => {
+                    console.error(err)
+                })
+            },
+            show(index) {
+                this.$Modal.info({
+                    title: 'User Info',
+                    content: `id：${this.totalData[index].id}<br>wx_id：${this.totalData[index].wx_id}<br>phone：${this.totalData[index].phone}<br>licence_snapshot：${this.totalData[index].licence_snapshot}`
+                })
+            },
+            remove(index, row) {
+                this.showModal = true
+                this.optIndex = index
+                this.optId = row.id
+            },
+            changePage(p) {
+                this.currentPage = p
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
